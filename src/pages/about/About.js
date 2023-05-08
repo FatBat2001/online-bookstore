f07c0eab45bed3485f7346cde7774482d8bb32f2
FatBat2001 <adhamekhalede@gmail.com>
2023-05-08 20:12:41 +0300

    aboutPage donoe

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,7 +1,12 @@
+import './about.css';
 export const About = () => {
   return (
-    <div>
-      <h1>About Page ...</h1>
+    <div className="about-container">
+      <h1 className="about-heading">About Online Bookstore</h1>
+      <p className="about-text">Welcome to Online Bookstore, your one-stop-shop for all your literary needs. We offer a wide selection of books, from bestsellers to rare finds, and we're constantly updating our inventory to ensure that you have access to the latest and greatest in literature.</p>
+      <p className="about-text">In addition to our vast selection of books, we also offer a variety of services to make your reading experience as enjoyable as possible. You can create an account and borrow books for a specified period of time. You can also rate and review books and see what other book lovers are reading and recommending.</p>
+      <p className="about-text">Our mission is to make reading accessible and enjoyable for everyone, and we're always looking for ways to improve our service. If you have any suggestions or feedback, please don't hesitate to contact us.</p>
+      <p className="about-text">Thank you for choosing Online Bookstore for all your reading needs.</p>
     </div>
   );
 };

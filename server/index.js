f072bde23cdcf6fad93b22900ec529c75e8586b5
FatBat2001 <adhamekhalede@gmail.com>
2023-05-08 21:33:17 +0300

    added server page on the host 4000

--- server/index.js
+++ server/index.js
@@ -12,9 +12,14 @@ app.use(cors());
 const auth = require("./routes/Auth");
 const books = require("./routes/Books");
 const admin = require("./routes/Admin")
+app.get('/', (req, res)=>{
+    res.send('hello, world');
+});
+
 //create server
 app.listen(4000,"localhost" , () => {
     console.log("SERVER IS RUNNING..");
+    
 });
 app.use("/admin",admin);
 app.use("/auth",auth);
